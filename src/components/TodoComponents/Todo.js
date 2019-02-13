b208e20e138ddd2d6e52e9a62f34fd40d66c6c75
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-02-13 12:47:21 +0000

    MVP: Created basic Todo component

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+const Todo = () => <div>{task}</div>;
+
+Todo.propTypes = {
+  task: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  completed: PropTypes.bool.isRequired,
+};
+
+export default Todo;
